7964dd1cfebc8df84c1fd53c91d0488260e2d77e
AnastasyMeleshko <7634907@mail.ru>
2021-12-09 01:44:18 +0300

    added corrections and new ways of solutions

--- task-05-01/app.js
+++ task-05-01/app.js
@@ -4,15 +4,45 @@ https://ru.wikipedia.org/wiki/%D0%A7%D0%B8%D1%81%D0%BB%D0%B0_%D0%A4%D0%B8%D0%B1%
 fibonacci(4) -> 0, 1, 1, 2
 */
 
+// 1 способ
+
 let initialArray = [];
 function fibonacci(n) {
-    for (let i = 0; i < n; i++) {
-        if (i >= 2) {
-            initialArray[i] = initialArray[i - 2] + initialArray[i - 1];
-        } else {
-            initialArray[i] = i;
+    if (n !== 0) {
+        for (let i = 0; i < n; i++) {
+            if (i === 0 || i === 1) {
+                initialArray[i] = i;
+            } else {
+                initialArray[i] = initialArray[i - 2] + initialArray[i - 1];
+            }
         }
+        console.log(initialArray.join(", "));
+    } else {
+        console.log("Последовательность чисел должна быть больше 0");
     }
-    console.log(initialArray.join(", "));
+
 }
-fibonacci(6);
+fibonacci(0);
+fibonacci(1);
+fibonacci(2);
+fibonacci(9);
+
+// 2 способ
+
+function fibo(n) {
+    if (n === 0) {
+        return "Последовательность чисел должна быть больше 0";
+    }
+    if(n === 1) {
+        return [0];
+    }
+    const result = [0,1];
+    for(let i = 2; i < n; i++) {
+        result[i] = result[i-2] + result[i-1];
+    }
+    return result;
+}
+console.log(fibo(0))
+console.log(fibo(1))
+console.log(fibo(2))
+console.log(fibo(9))
--- task-05-02/app.js
+++ task-05-02/app.js
@@ -3,14 +3,16 @@
 uniqueFunc([1,5,2,3,4,3,2,4,1,5,6]) -> [1,5,2,3,4,6]
 */
 
+// 1 способ
+
 initialArray = [1, 5, 2, 3, 4, 3, 2, 4, 1, 5, 6];
-let n = initialArray.length;
+let arrayLength = initialArray.length;
 console.log("Исходный массив: " + initialArray);
 resultArray = [];
 
 function uniqueFunc() {
 
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < arrayLength; i++) {
         let shifted = initialArray.shift();
         if (initialArray.includes(shifted)) {
             initialArray.splice(initialArray.indexOf(shifted), 1);
@@ -23,3 +25,14 @@ function uniqueFunc() {
 }
 
 uniqueFunc();
+
+// 2 способ
+
+let arr = [1, 5, 2, 3, 4, 3, 2, 4, 1, 5, 6];
+let resultArr = [];
+for (let number of arr) {
+    if(!resultArr.includes(number)) {
+        resultArr.push(number)
+    }
+}
+console.log(resultArr);
