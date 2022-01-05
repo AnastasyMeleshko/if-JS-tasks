c50f322da292520d2deaa5766ec814cd80544012
AnastasyMeleshko <7634907@mail.ru>
2022-01-05 20:15:44 +0300

    add final correction

--- task-10/main.js
+++ task-10/main.js
@@ -6,18 +6,14 @@ For example, given [3,2,1,5,6,4] and k = 2, return 5.
 */
 
 let givenArray = [441777,1,2,89,4,422,3];
-let k = 1;
+let k = 2;
 
 function largestByIndex(givenArray, index) {
     if (index > givenArray.length || index <= 0) {
         return `Введите корректный индекс`;
     }
-    else {
-        let sortedArray = givenArray.sort(function(a, b) {
-            return a - b;
-        });
-        return sortedArray.at(-index);
-    }
+    const sortedArray = givenArray.sort((a, b) => a - b);
+    return sortedArray.at(-index);
 }
 
 console.log(largestByIndex(givenArray, k));
