a6eb3e03929892cd4939d1345fb3b981b8a44fab
AnastasyMeleshko <7634907@mail.ru>
2022-01-05 18:58:09 +0300

    add correction

--- task-10/main.js
+++ task-10/main.js
@@ -6,7 +6,7 @@ For example, given [3,2,1,5,6,4] and k = 2, return 5.
 */
 
 let givenArray = [441777,1,2,30,4,422,3];
-let k = 2;
+let k = 4;
 
 function largestByIndex(givenArray, index) {
     console.log(`Дан массив: ${givenArray}`);
@@ -19,4 +19,4 @@ function largestByIndex(givenArray, index) {
     }
 }
 
-console.log(largestByIndex(givenArray, 4));
+console.log(largestByIndex(givenArray, k));
