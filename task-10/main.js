2504cb526f127ec3c365d56695d12ff5bcc6389d
AnastasyMeleshko <7634907@mail.ru>
2022-01-05 19:39:43 +0300

    add corrections

--- task-10/main.js
+++ task-10/main.js
@@ -5,18 +5,20 @@ not the kth distinct element.
 For example, given [3,2,1,5,6,4] and k = 2, return 5.
 */
 
-let givenArray = [441777,1,2,30,4,422,3];
-let k = 4;
+let givenArray = [441777,1,2,89,4,422,3];
+let k = 1;
 
 function largestByIndex(givenArray, index) {
-    console.log(`Дан массив: ${givenArray}`);
-    let sortedArray = givenArray.sort();
-    if (index < givenArray.length && index !== 0) {
-        return `${index}-й с конца наибольший элемент:` + sortedArray[sortedArray.length - index];
+    if (index > givenArray.length || index <= 0) {
+        return `Введите корректный индекс`;
     }
-   else {
-       return `Введите корректный индекс`;
+    else {
+        let sortedArray = givenArray.sort(function(a, b) {
+            return a - b;
+        });
+        return sortedArray.at(-index);
     }
 }
 
 console.log(largestByIndex(givenArray, k));
+
